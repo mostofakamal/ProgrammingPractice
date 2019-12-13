2b8fd5d969063bf56765b2c9fba313dbc0b5a881
Mostofa Kamal <mostofa.cefalo@hotmail.com>
2019-12-13 20:48:53 +0600

    Added code for Foo barprinting

---
+++ ProgrammingPractice/FooBar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBar
+{
+    class Program
+    {
+        public static void PrintNumberWithFooBar()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                var currentNumberString = i.ToString();
+                if (i % 3 == 0)
+                {
+                    Console.Write("Foo");
+                    currentNumberString = "";
+                }
+                if (i % 5 == 0)
+                {
+                    Console.Write("Bar");
+                    currentNumberString = "";
+                }
+                Console.WriteLine(currentNumberString);
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintNumberWithFooBar();
+        }
+    }
+}
